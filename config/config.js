87dd3af418009c74f652a671c717788ac1f77856
adrian salona <Adrian>
2023-04-15 12:48:16 +0200

    Feat: connection to the database has been made

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": "root",
-    "database": "techVision_ecommerce",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql",
-    "port" : 3306
+    "port": process.env.MYSQL_PORT
   },
   "test": {
     "username": "root",
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('./db/db');
+require('dotenv').config();
+//const routes = require("./router");
+const app = express();
+
+app.use(express.json());
+//app.use(routes)
+
+//PORT
+const PORT = 3000;
+
+  db.then(() => {
+    //Starting server
+    app.listen(PORT, () => console.log("Server on port " + PORT));
+})
+    .catch((err) => console.log(err.message)); 
