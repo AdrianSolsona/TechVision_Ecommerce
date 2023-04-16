7039ea17779b08f7618cdfb32cb4a1a5301ef73e
adrian salona <Adrian>
2023-04-16 12:15:27 +0200

    Feat: middleware for general verification is performed in the app

---
+++ middleware/verifyToken.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    try {
+
+        const authorization = req.headers.authorization;
+
+        //if the authorization with the token exists
+        if (!authorization) {
+            return res.send('Invalid token');
+        }
+        //Separate the token from the bearer
+        const [strategy, token] = authorization.split(" ");
+        //Verify the token with your secret
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.userId = decoded.userId;
+        req.rolId = decoded.rolId;
+        req.email = decoded.email;
+
+        next();
+        //Pick up the possible error
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+}
+
+module.exports = verifyToken;
