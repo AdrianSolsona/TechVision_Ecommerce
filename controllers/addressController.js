9a7ce4750c03c80d282d83d87da6f965bcb538d4
adrian salona <Adrian>
2023-04-17 18:29:14 +0200

    Feat: realized the drivers for modification and deletion of addresses

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -33,4 +33,135 @@ addressController.createAddress = async (req, res) => {
     }
 };
 
+addressController.getAddress = async (req, res) => {
+
+    try{
+
+        const addressId = req.userId;
+
+        const address = await Address.findAll(
+            {
+                where: 
+                {
+                    user_id: addressId
+                },
+                attributes: {
+                    exclude: [ "user_id", "createdAt", "updatedAt"]
+                }
+            })
+
+        if (!address){
+            return res.send("User Not Found")
+        }
+
+        return res.json(address);
+    
+    }catch(error){
+        return res.status(500).send(error.message)
+    }   
+};
+/*
+addressController.putAddress = async (req, res) =>{
+
+    try{
+    
+        const addressId = req.addressId
+    
+        const { name, surname, phone,address, country, city, postcode } = req.body;
+    
+        const updateAddress = await Address.update({
+            name : name,
+            surname : surname,
+            address : address,
+            phone : phone,
+            country: country,
+            city : city,
+            postcode : postcode}, {where:{id:addressId}})
+    
+        return res.json(updateAddress)
+    
+    }catch(error){
+    
+        return res.status(500).send(error.message)
+    }
+    };
+*/
+/*
+addressController.putAddress = async (req, res) => {
+    try {
+      const addressId = req.params.addressId; // Se utiliza params para obtener el ID de la dirección a actualizar
+      const userId = req.user.id; // Se obtiene el ID del usuario autenticado a través del token
+  
+      const { name, surname, phone, address, country, city, postcode } = req.body;
+  
+      // Se busca la dirección a actualizar verificando que pertenece al usuario autenticado
+      const foundAddress = await Address.findOne({
+        where: { id: addressId, userId: userId },
+      });
+  
+      if (!foundAddress) {
+        return res.status(404).json({ message: "Address not found" });
+      }
+  
+      // Se actualiza la información de la dirección
+      const updatedAddress = await foundAddress.update({
+        name: name,
+        surname: surname,
+        address: address,
+        phone: phone,
+        country: country,
+        city: city,
+        postcode: postcode,
+      });
+  
+      return res.json(updatedAddress);
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  };*/
+
+
+    addressController.putAddress = async (req, res) =>{
+
+    try{
+
+        const addressId = req.params.id
+
+        const {name,surname, phone, address, country, city, postcode} = req.body;
+
+        const updateAddress = await Address.update({
+            name: name,
+            surname: surname,
+            address: address,
+            phone: phone,
+            country: country,
+            city: city,
+            postcode: postcode,}, {where:{id:addressId}})
+
+        return res.json(updateAddress)
+
+    }catch(error){
+
+        return res.status(500).send(error.message)
+    }
+}
+
+addressController.deleteAddress = async (req, res) => {
+
+    try{
+
+        const addressId = req.params.id
+    
+        const deleteAddress = await Address.destroy({where: { id: addressId}})
+
+        return res.json(deleteAddress);
+
+    }catch(error){
+
+        return res.status(500).send(error.message)
+    }
+};
+  
+  
+
 module.exports = addressController
--- views/addressRoutes.js
+++ views/addressRoutes.js
@@ -4,10 +4,8 @@ const router = require("express").Router();
 
 // All available routes for the model User
 router.post("/address",addressController.createAddress)
-/*
-router.get("/address/all", verifyToken,addressController.getAddress)
 router.get("/address", verifyToken,addressController.getAddress)
-router.put("/address", verifyToken,addressController.putAddress)
-router.delete("/address", verifyToken, addressController.deleteAddress)
-*/
+router.put("/address/:id", verifyToken,addressController.putAddress)
+router.delete("/address/:id", verifyToken, addressController.deleteAddress)
+
 module.exports = router
